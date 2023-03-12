9bc682ea647f76b34a8f8e8155d74df6204f5f1d
Brittany Boughter <brittany.wood0308@yahoo.com>
2023-03-12 12:35:36 -0700

    added if statement for lowercase letters

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+// var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// // function writePassword() {
+// //   var password = generatePassword();
+// //   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+// //   passwordText.value = password;
 
-}
+// }
 function generatePassword() {
-  var numbers = ['0', '1' , '2', '3', '4', '5', '6', '7', '8', '9'];
+  var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
   var lowercaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-  var uppercaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+  var uppercaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  var specialCharacters = ['!', '`', '~', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', '{', ']', '}', ';', ':', '"', ',', '<', '.', '>', '/', '?', '|'];
+  var possibleCharacters = [];
 }
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+numberOfCharacters = prompt("How many characters do you want in your password? Please choose a number between 8 and 128.");
+if (numberOfCharacters < 8 || numberOfCharacters > 128) {
+  return "Please choose a number between 8 and 128."
+} else {
+  alert("Your password will be " + numberOfCharacters + " characters long.");
+}
+lowercase = confirm('Do you want lowercase letters?');
+   if(true) text = "Your password will include lowercase letters"; {
+    };
+
+
