42e48569a18ef4fec21c71ec8b24a7d4ec910394
Brittany Boughter <brittany.wood0308@yahoo.com>
2023-03-13 16:48:01 -0700

    fixed alerts for the first if statements

--- script.js
+++ script.js
@@ -1,63 +1,71 @@
 
+var numbers = '0123456789'
+var lowercaseLetters = 'abcdefghijklmnopqrstuvwxyz'
+var uppercaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+var specialCharacters = '!`~@#$%^&*()-_=+[{]};:",<./ ? |';
+var possibleCharacters = [];
+var finalPassword = [];
+
 function generatePassword() {
-  var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-  var lowercaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-  var uppercaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-  var specialCharacters = ['!', '`', '~', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', '{', ']', '}', ';', ':', '"', ',', '<', '.', '>', '/', '?', '|'];
-  var possibleCharacters = [];
 
-numberOfCharacters = prompt("How many characters do you want in your password? Please choose a number between 8 and 128.");
-if (numberOfCharacters < 8 || numberOfCharacters > 128) {
-  alert("Please choose a number between 8 and 128.")
-} else {
-  alert("Your password will be " + numberOfCharacters + " characters long.");
-}
 
-lowercase = confirm('Do you want lowercase letters?');
-if (!lowercase) alert("Your password will NOT include lowercase letters."); {
-}
+  numberOfCharacters = prompt("How many characters do you want in your password? Please choose a number between 8 and 128.");
+  if (numberOfCharacters < 8 || numberOfCharacters > 128) {
+    alert("Please choose a number between 8 and 128.");
+    generatePassword();
+  }
 
-uppercase = confirm('Do you want uppercase letters?');
-if (!uppercase) alert = 'Your password will NOT include uppercase letters.'; {
-}
+  let lowercase = confirm('Do you want lowercase letters?');
+  if (!lowercase) alert("Your password will NOT include lowercase letters."); {
+  }
 
-special = confirm('Do you want to include special characters?');
-if (!special) alert = 'Your password will NOT include special characters.'; {
-}
+  let uppercase = confirm('Do you want uppercase letters?');
+  if (!uppercase) alert('Your password will NOT include uppercase letters.'); {
+  }
 
-includeNumbers = confirm('Do you want to include numbers?')
-if (!includeNumbers) alert = 'Your password will NOT include numbers.'; {
-}
+  let special = confirm('Do you want to include special characters?');
+  if (!special) alert('Your password will NOT include special characters.'); {
+  }
 
-if (includeNumbers === false && lowercase === false && uppercase === false && special === false) {
-  alert("You must choose at lease one.")
-}
+  let includeNumbers = confirm('Do you want to include numbers?')
+  if (!includeNumbers) alert('Your password will NOT include numbers.'); {
+  }
 
-if (includeNumbers === true) {
-  possibleCharacters = possibleCharacters.concat(numbers)
-}
-if (lowercase === true) {
-  possibleCharacters= possibleCharacters.concat(lowercaseLetters)
-}
-if (uppercase === true) {
-  possibleCharacters = possibleCharacters.concat(uppercaseLetters)
-}
-if (special === true) {
-  possibleCharacters = possibleCharacters.concat(specialCharacters)
-}
+  if (includeNumbers === false && lowercase === false && uppercase === false && special === false) {
+    alert("You must choose at lease one.")
+    generatePassword()
+  }
 
-var password;
-for (let i = 0; i < numberOfCharacters; i++) {
-  var possibleResults = [Math.floor(Math.random() * possibleCharacters.length)];
-  password = password + possibleCharacters[possibleResults]
-}
-  return password;
+  if (includeNumbers) {
+
+    possibleCharacters.push(...numbers);
+  }
+  console.log(possibleCharacters, "possible characters line 39");
+  if (lowercase) {
+    possibleCharacters.push(...lowercaseLetters)
+
+  }
+  if (uppercase) {
+    possibleCharacters.push(...uppercaseLetters)
+
+  }
+  if (special) {
+    possibleCharacters.push(...specialCharacters)
+
+  }
+
+
+  for (let i = 0; i < numberOfCharacters; i++) {
+    finalPassword.push(possibleCharacters[Math.floor(Math.random() * possibleCharacters.length)])
+
+  }
+  return finalPassword;
 }
 
 var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  passwordText.value = password;
+  passwordText.value = password.join('');
 }
 generateBtn.addEventListener("click", writePassword)
